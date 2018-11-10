23ef08fbb97e0c209f6379794953dff7643f3964
eui won kim <euiwonkim@euis-MacBook-Pro.local>
2018-11-10 00:00:22 -0800

    initialize repo with app base

---
+++ app/src/main/java/com/example/euiwonkim/githubchallenge/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.euiwonkim.githubchallenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
