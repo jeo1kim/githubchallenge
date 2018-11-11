78a545f8c19e6e2fa938f0b0c8a165cce9805118
eui won kim <euiwonkim@euis-MacBook-Pro.local>
2018-11-10 23:07:50 -0800

    highlight remove and added changes in the textView

--- app/src/main/java/com/example/euiwonkim/githubchallenge/DiffViewActivity.java
+++ app/src/main/java/com/example/euiwonkim/githubchallenge/DiffViewActivity.java
@@ -2,7 +2,13 @@ package com.example.euiwonkim.githubchallenge;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.BackgroundColorSpan;
+import android.text.style.ForegroundColorSpan;
 import android.widget.TextView;
 
 
@@ -14,6 +20,9 @@ import android.widget.TextView;
 public class DiffViewActivity extends Activity {
 
     private String diffString;
+
+    private String diffRemoved;
+    private String diffAdded;
 
     TextView diffLeftTextView;
     TextView diffRightTextView;
@@ -29,7 +38,7 @@ public class DiffViewActivity extends Activity {
 
         Intent intent = getIntent();
         diffString = intent.getStringExtra("diff");
-
+        makeDiff(diffString);
     }
 
     @Override
@@ -37,10 +46,77 @@ public class DiffViewActivity extends Activity {
         super.onStart();
 
 
-        diffLeftTextView.setText(diffString);
-        diffRightTextView.setText(diffString);
+        diffLeftTextView.setText(Html.fromHtml(diffRemoved), TextView.BufferType.SPANNABLE);
+        diffRightTextView.setText(Html.fromHtml(diffAdded), TextView.BufferType.SPANNABLE);
 
 
+    }
+
+    /**
+     * Functoin to separate the removed and added changes in the pull request's diff file.
+     *
+     * @param diff
+     */
+    protected void makeDiff(String diff) {
+
+        String[] lines = diff.split("\\r?\\n");
+
+        StringBuilder removed = new StringBuilder();
+        StringBuilder added = new StringBuilder();
+
+        // reconstruct the remove and added diff texts
+        for(String line : lines) {
+            int len = line.length()-1;
+            if(line.charAt(0) == '-'){
+
+                // highlight with html
+                String str = "<span style='background-color: #F08080'>"+ line +"</span>";
+
+                removed.append(str);
+                removed.append("<br>");
+                // replace + lines with padding
+                String pad = paddChars(line);
+                added.append(pad);
+            }else if(line.charAt(0) == '+'){
+
+                // do some highlightins
+                String str = "<span style='background-color: #98FB98'>"+ line +"</span>";
+
+                added.append(str);
+                added.append("<br>");
+
+                // replace - lines with padding
+                String pad = paddChars(line);
+                removed.append(pad);
+            }else{
+                // just append other lines normally
+                removed.append(line);
+                added.append(line);
+                removed.append("<br>");
+                added.append("<br>");
+            }
+
+        }
+
+        diffRemoved = removed.toString();
+        diffAdded = added.toString();
+
+    }
+
+
+    protected String paddChars(String s) {
+
+        String paddingCharacter = "  ";
+        StringBuffer padding = new StringBuffer();
+
+        while(padding.length() < s.length() ){
+            padding.append(paddingCharacter);
+        }
+        padding.append("<br>");
+        if(s.length() > 60) {
+            padding.append("<br>");
+        }
+        return padding.toString();
     }
 
 }
